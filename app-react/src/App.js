f39160558ee660bc4d3950b62038f89e55a49071
josh <joshdanielsaraa@gmail.com>
2024-12-06 09:40:35 +0800

    hash router

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
 import SignUp from './SignUp';
 import Dashboard from './Dashboard';
@@ -7,15 +7,34 @@ import { auth } from './firebase';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 const App = () => {
-  const [user] = useAuthState(auth);
+  const [user] = useAuthState(auth); // Firebase hook to manage authentication state
 
   return (
     <Router>
       <Routes>
-        <Route path="/login" element={user ? <Navigate to="/dashboard" /> : <Login />} />
-        <Route path="/signup" element={user ? <Navigate to="/dashboard" /> : <SignUp />} />
-        <Route path="/dashboard" element={user ? <Dashboard /> : <Navigate to="/login" />} />
-        <Route path="*" element={<Navigate to="/login" />} />
+        {/* Login route */}
+        <Route 
+          path="/login" 
+          element={user ? <Navigate to="/dashboard" /> : <Login />} 
+        />
+        
+        {/* Sign Up route */}
+        <Route 
+          path="/signup" 
+          element={user ? <Navigate to="/dashboard" /> : <SignUp />} 
+        />
+        
+        {/* Dashboard route */}
+        <Route 
+          path="/dashboard" 
+          element={user ? <Dashboard /> : <Navigate to="/login" />} 
+        />
+        
+        {/* Default redirect if no route matches */}
+        <Route 
+          path="*" 
+          element={<Navigate to="/login" />} 
+        />
       </Routes>
     </Router>
   );
