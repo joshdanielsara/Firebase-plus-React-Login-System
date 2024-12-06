7c65c649b219bf376e01319e0aee670034f4ace2
josh <joshdanielsaraa@gmail.com>
2024-12-06 22:07:03 +0800

    test

--- app-react/src/App.js
+++ app-react/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Login from './Login';
 import SignUp from './SignUp';
 import Dashboard from './Dashboard';
