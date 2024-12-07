28965d5a409ad1aca5fafedc855c28baa6662b52
josh <joshdanielsaraa@gmail.com>
2024-12-07 22:03:27 +0800

    test

--- app-react/src/Login.js
+++ app-react/src/Login.js
@@ -34,7 +34,7 @@ const Login = () => {
               className="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
             />
           </div>
-          <div>
+           <div>
             <label className="block text-sm font-medium text-gray-700 mb-1">Password</label>
             <input
               type="password"
